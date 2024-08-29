4314e60bdf806e0258dd22a67efcbd70f2eafcd9
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-29 13:52:14 +0200

    feat: add Header component + About Us and Blog pages

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Header from "@/components/header";
+
+export default function Home() {
+  console.log('ciao');
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p><Link href="/about">About Us</Link></p>
+      <p><Link href="/blog">Blog</Link></p>
+    </main>
+  );
+}
